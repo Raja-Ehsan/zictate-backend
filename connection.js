036def8adc3d6153f00ecd1384dd0a46316f79c3
rajaehsan.0022@gmail.com <91175440+rajaehsan1234@users.noreply.github.com>
2022-08-02 07:52:32 -0700

    make it better

--- connection.js
+++ connection.js
@@ -1,15 +1,34 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host     : 'us-cdbr-east-06.cleardb.net',
-  user     : 'b79df9c093fe0d',
-  password : '0687e529',
-  database : 'heroku_d764cdfbd769019'
-});
 
-connection.connect((err) => {
-    if(err) throw err;
-    console.log('Connected to MySQL Server!');
-});
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({
+    host     : 'us-cdbr-east-06.cleardb.net',
+    user     : 'b79df9c093fe0d',
+    password : '0687e529',
+    database : 'heroku_d764cdfbd769019'
+  }); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+handleDisconnect()
+
 
 module.exports=connection;
